140d2d7b94ef2055d918ff449e87772472211cc8
abdulsametkara <abdulsamedkra@gmail.com>
2025-05-04 14:20:39 +0300

    fix
    project cleaned

--- 8-run-app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@setlocal enableextensions
-@cd /d "%~dp0"
-
-echo Running Application
-java -jar music-app/target/music-app-1.0-SNAPSHOT.jar
-
-echo Operation Completed!
-pause
--- run-fx-app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo JavaFX App Starting...
-
-cd music-app
-mvn clean javafx:run -DskipTests
-
-echo App Closed!
-pause 
